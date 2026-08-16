5b20113b4c1e9a09345e53e19bf32a7addb3979b
Malinkina-Nadejhda <nadekmalek@gmail.com>
2026-08-17 00:41:29 +0300

    Add verbose_name.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -41,7 +41,11 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     lat = models.FloatField(verbose_name="Широта")
     lon = models.FloatField(verbose_name="Долгота")
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name="Покемон"
+    )
 
     appeared_at = models.DateTimeField(
         null=True,
