ffaf99aa9f5270d8034fc0d6b7f83337909eed5d
Malinkina-Nadejhda <nadekmalek@gmail.com>
2026-08-18 23:17:22 +0300

    Add related_name to pokemon field in PokemonEntity.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -44,7 +44,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        verbose_name="Покемон"
+        verbose_name="Покемон",
+        related_name="entities"
     )
 
     appeared_at = models.DateTimeField(
